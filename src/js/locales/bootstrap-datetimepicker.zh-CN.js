9fc625f800b2c6c1275ae2c29c55b06efb227dcf
Feilong Niu <niufeilong@wisdomgarden.com>
2017-04-09 00:26:03 +0800

    fix moment
    git checkout 3.1.4 src/js/bootstrap-datetimepicker.js

--- src/js/locales/bootstrap-datetimepicker.zh-CN.js
+++ src/js/locales/bootstrap-datetimepicker.zh-CN.js
@@ -6,8 +6,6 @@
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
         define(['moment'], factory); // AMD
-    } else if (typeof exports === 'object') {
-        module.exports = factory(require('../moment')); // Node
     } else {
         factory(window.moment); // Browser global
     }
